7911639c77d259b8a98a5530d5f75a3323b10a66
Lucian Andercou <lucian.andercou@gmail.com>
2017-03-10 00:37:17 +0200

    Removed useless if- once used for debug

--- main.js
+++ main.js
@@ -10,10 +10,6 @@ app.controller('BandData', function ($scope, $http) {
         console.log(Flask.url_for('home') +'gigs?'+ item_no_space)
         $http.get(Flask.url_for('home') +'gigs?'+ item_no_space).success(function (response) {
                 $scope.locations = response.data;
-                if (typeof $scope.locations == 'object')
-                    console.log($scope.locations);
-                else
-                    console.log('Not actually an object, it really is ', typeof $scope.greeting);
             });
     };
 });
